4eb15df3fbf18556c755f4ae72284ccdeec1c25f
Emil Mosbæk Walsøe Pedersen <emil7213@edu.easj.dk>
2019-05-02 11:08:24 +0200

    Added summaries to Model

--- RFStorage/Model/Organisation.cs
+++ RFStorage/Model/Organisation.cs
@@ -10,6 +10,9 @@ namespace RFStorage.Model
     class Organisation
     {
         #region Properties
+        /// <summary>
+        /// Properties til Organisation, Get/Set sørger for at hente eller sætte værdierne på instancefields med unikke for objekter.
+        /// </summary>
         public int OrganisationID { get; set; }
         public int OrganisationNavn { get; set; }
         public ObservableCollection<Vare> UdleveretVareOC { get; set; }
@@ -18,8 +21,11 @@ namespace RFStorage.Model
 
 
         #region Construktor
-        
-        
+        /// <summary>
+        /// /// Constructors enable the programmer to set default values, limit instantiation, and write code that is flexible and easy to read.
+        /// </summary>
+        /// <param name="organisationId"></param>
+        /// <param name="organisationNavn"></param>
         public Organisation(int organisationId, int organisationNavn)
         {
             OrganisationID = organisationId;
