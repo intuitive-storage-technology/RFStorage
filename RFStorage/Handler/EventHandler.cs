b97d0b6a20ea8ab1fc21daaf56c7fcbac175070b
Jon Lam <jonpawlam@gmail.com>
2019-05-08 11:53:48 +0200

    Fix Handler Mangler Selected Bug

--- RFStorage/Handler/EventHandler.cs
+++ RFStorage/Handler/EventHandler.cs
@@ -11,58 +11,66 @@ namespace RFStorage.Handler
 {
     class EventHandler
     {
-        public LagerstatusVM LagerstatusVM { get; set; }
+        //public LagerstatusVM LagerstatusVM { get; set; }
 
-        public EventHandler(LagerstatusVM lagerstatusVM)
-        {
-            LagerstatusVM = lagerstatusVM;
-        }
+        //public EventHandler(LagerstatusVM lagerstatusVM)
+        //{
+        //    LagerstatusVM = lagerstatusVM;
+        //}
 
-        public void CreateVare()
-        {
-            //var newEvent = new Event(EventViewModel.Id, EventViewModel.Name, EventViewModel.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel.Date, EventViewModel.Time), EventViewModel.Description);
-            //EventViewModel.EventCatalogSingleton.Add(newEvent);
-           //LagerstatusVM.LagerstatusSingleton.VareOC.Add(LagerstatusVM.VareID, LagerstatusVM.Antal, LagerstatusVM.VareNavn, LagerstatusVM.VareType, LagerstatusVM.VareTilstand);
+        //public void CreateVare()
+        //{
+        //    //var newEvent = new Event(EventViewModel.Id, EventViewModel.Name, EventViewModel.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel.Date, EventViewModel.Time), EventViewModel.Description);
+        //    //EventViewModel.EventCatalogSingleton.Add(newEvent);
+        //   //LagerstatusVM.LagerstatusSingleton.VareOC.Add(LagerstatusVM.VareID, LagerstatusVM.Antal, LagerstatusVM.VareNavn, LagerstatusVM.VareType, LagerstatusVM.VareTilstand);
+           
+        //    LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test1",1,"type",1,"Supergod"));
+        //    LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test2", 1, "type", 1, "Supergod"));
+        //    LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test3", 1, "type", 1, "Supergod"));
+        //    LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test4", 1, "type", 1, "Supergod"));
 
-            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test",1,"type",1,"Supergod"));
-            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test", 1, "type", 1, "Supergod"));
-            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test", 1, "type", 1, "Supergod"));
-            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test", 1, "type", 1, "Supergod"));
+        //    // EventViewModel.EventCatalogSingleton.Add(EventViewModel.Id, EventViewModel.Name, EventViewModel.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel.Date, EventViewModel.Time), EventViewModel.Description);
+        //}
 
-            // EventViewModel.EventCatalogSingleton.Add(EventViewModel.Id, EventViewModel.Name, EventViewModel.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(EventViewModel.Date, EventViewModel.Time), EventViewModel.Description);
-        }
+        //public void SetSelectedVare(Vare vare)
+        //{
+            
+        //    LagerstatusVM.SelectedEvent = vare;
+        //}
 
-        public void SetSelectedVare(Vare vare)
-        {
-            LagerstatusVM.SelectedEvent = vare;
-        }
+        //public void DeleteVare()
+        //{
+        //    if (LagerstatusVM.SelectedEvent != null)
+        //    {
+        //        LagerstatusVM.LagerstatusSingleton.VareOC.Remove(LagerstatusVM.SelectedEvent);
+        //    }
+            
+        //    ////new UICommandInvokedHandler(this.CommandInvokedHandler);
 
-        public async void DeleteVare()
-        {
+        //    //// Create the message dialog and set its content
+        //    //var messageDialog = new MessageDialog("Are you sure you want to Delete the Event: " + LagerstatusVM.SelectedEvent.VareNavn + " ?");
 
-            // Create the message dialog and set its content
-            var messageDialog = new MessageDialog("Are you sure you want to Delete the Event: " + LagerstatusVM.SelectedEvent.VareNavn + " ?");
+        //    //// Add commands and set their callbacks; both buttons use the same callback function instead of inline event handlers
+        //    //messageDialog.Commands.Add(new UICommand("Yes", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+        //    //messageDialog.Commands.Add(new UICommand("No", null));
 
-            // Add commands and set their callbacks; both buttons use the same callback function instead of inline event handlers
-            messageDialog.Commands.Add(new UICommand("Yes", new UICommandInvokedHandler(this.CommandInvokedHandler)));
-            messageDialog.Commands.Add(new UICommand("No", null));
+        //    //// Set the command that will be invoked by default
+        //    //messageDialog.DefaultCommandIndex = 0;
 
-            // Set the command that will be invoked by default
-            messageDialog.DefaultCommandIndex = 0;
+        //    //// Set the command to be invoked when escape is pressed
+        //    //messageDialog.CancelCommandIndex = 1;
 
-            // Set the command to be invoked when escape is pressed
-            messageDialog.CancelCommandIndex = 1;
-
-            // Show the message dialog
-            await messageDialog.ShowAsync();
+        //    //// Show the message dialog
+        //    //await messageDialog.ShowAsync();
 
 
 
-        }
 
-        private void CommandInvokedHandler(IUICommand command)
-        {
-            LagerstatusVM.LagerstatusSingleton.VareOC.Remove(LagerstatusVM.SelectedEvent);
-        }
+        //}
+
+        //private void CommandInvokedHandler(IUICommand command)
+        //{
+        //    LagerstatusVM.LagerstatusSingleton.VareOC.Remove(LagerstatusVM.SelectedEvent);
+        //}
     }
 }
---
+++ RFStorage/Handler/VareHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using RFStorage.Model;
+using RFStorage.ViewModel;
+
+namespace RFStorage.Handler
+{
+    class VareHandler
+    {
+        //
+        public LagerstatusVM LagerstatusVM { get; set; }
+
+        //Construktor
+        public VareHandler(LagerstatusVM lagerstatusVm)
+        {
+            LagerstatusVM = lagerstatusVm;
+        }
+
+        //methods
+
+        //Create
+        public void CreateVare()
+        {
+            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test1", 1, "type", 1, "Supergod"));
+            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test2", 1, "type", 1, "Supergod"));
+            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test3", 1, "type", 1, "Supergod"));
+            LagerstatusVM.LagerstatusSingleton.VareOC.Add(new Vare("Test4", 1, "type", 1, "Supergod"));
+            
+        }
+
+        public void SetSelectedVare(Vare vare)
+        {
+            LagerstatusVM.SelectedVare = vare;
+        }
+
+        public void DeleteVare()
+        {
+            if (LagerstatusVM.SelectedVare != null)
+            {
+                LagerstatusVM.LagerstatusSingleton.VareOC.Remove(LagerstatusVM.SelectedVare);
+            }
+        }
+
+        private void CommandInvokedHandler(IUICommand command)
+        {
+            LagerstatusVM.LagerstatusSingleton.Remove(LagerstatusVM.SelectedVare);
+        }
+
+    }
+}
