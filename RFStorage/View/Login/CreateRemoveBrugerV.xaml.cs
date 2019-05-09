5e78d925291ce734309da23488af012896208091
Cerbera <45389499+Slackertron@users.noreply.github.com>
2019-05-09 10:38:08 +0200

    Revert "Revert "Jon kan slettes""

--- RFStorage/View/Login/CreateRemoveBrugerV.xaml.cs
+++ RFStorage/View/Login/CreateRemoveBrugerV.xaml.cs
@@ -20,9 +20,9 @@ namespace RFStorage.View.Login
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class CreateRemoveUserV : Page
+    public sealed partial class CreateRemoveBrugerV : Page
     {
-        public CreateRemoveUserV()
+        public CreateRemoveBrugerV()
         {
             this.InitializeComponent();
         }
