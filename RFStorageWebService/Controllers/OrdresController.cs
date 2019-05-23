f33f40c90f2a764c962765f15dcf1f2becfd758f
CelineSten <celi4162@edu.easj.dk>
2019-05-23 12:59:19 +0200

    Web Service

---
+++ RFStorageWebService/Controllers/OrdresController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RFStorageWebService;
+
+namespace RFStorageWebService.Controllers
+{
+    public class OrdresController : ApiController
+    {
+        private RFStorageContext db = new RFStorageContext();
+
+        // GET: api/Ordres
+        public IQueryable<Ordre> GetOrdre()
+        {
+            return db.Ordre;
+        }
+
+        // GET: api/Ordres/5
+        [ResponseType(typeof(Ordre))]
+        public IHttpActionResult GetOrdre(int id)
+        {
+            Ordre ordre = db.Ordre.Find(id);
+            if (ordre == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ordre);
+        }
+
+        // PUT: api/Ordres/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrdre(int id, Ordre ordre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != ordre.OrdreID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(ordre).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrdreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Ordres
+        [ResponseType(typeof(Ordre))]
+        public IHttpActionResult PostOrdre(Ordre ordre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Ordre.Add(ordre);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrdreExists(ordre.OrdreID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = ordre.OrdreID }, ordre);
+        }
+
+        // DELETE: api/Ordres/5
+        [ResponseType(typeof(Ordre))]
+        public IHttpActionResult DeleteOrdre(int id)
+        {
+            Ordre ordre = db.Ordre.Find(id);
+            if (ordre == null)
+            {
+                return NotFound();
+            }
+
+            db.Ordre.Remove(ordre);
+            db.SaveChanges();
+
+            return Ok(ordre);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrdreExists(int id)
+        {
+            return db.Ordre.Count(e => e.OrdreID == id) > 0;
+        }
+    }
+}
---
+++ RFStorageWebService/Controllers/OrganisationsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RFStorageWebService;
+
+namespace RFStorageWebService.Controllers
+{
+    public class OrganisationsController : ApiController
+    {
+        private RFStorageContext db = new RFStorageContext();
+
+        // GET: api/Organisations
+        public IQueryable<Organisation> GetOrganisation()
+        {
+            return db.Organisation;
+        }
+
+        // GET: api/Organisations/5
+        [ResponseType(typeof(Organisation))]
+        public IHttpActionResult GetOrganisation(int id)
+        {
+            Organisation organisation = db.Organisation.Find(id);
+            if (organisation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(organisation);
+        }
+
+        // PUT: api/Organisations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrganisation(int id, Organisation organisation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != organisation.OrganisationID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(organisation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrganisationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Organisations
+        [ResponseType(typeof(Organisation))]
+        public IHttpActionResult PostOrganisation(Organisation organisation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Organisation.Add(organisation);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrganisationExists(organisation.OrganisationID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = organisation.OrganisationID }, organisation);
+        }
+
+        // DELETE: api/Organisations/5
+        [ResponseType(typeof(Organisation))]
+        public IHttpActionResult DeleteOrganisation(int id)
+        {
+            Organisation organisation = db.Organisation.Find(id);
+            if (organisation == null)
+            {
+                return NotFound();
+            }
+
+            db.Organisation.Remove(organisation);
+            db.SaveChanges();
+
+            return Ok(organisation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrganisationExists(int id)
+        {
+            return db.Organisation.Count(e => e.OrganisationID == id) > 0;
+        }
+    }
+}
